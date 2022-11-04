147eac2d6cdd37bdfaa238ffea35ddd17f9dc434
Sangjin Lee <leesj2004@gmail.com>
2022-11-04 17:52:10 +0900

    새로운 사용법

--- snprintf/snprintf.cpp
+++ snprintf/snprintf.cpp
@@ -1,10 +1,25 @@
 /* snprintf example */
 #include <stdio.h>
 
+int test1()
+{
+	int len;
+	char data[256];
+
+
+	len = snprintf(data, sizeof(data), "%.*s", 6, "I love South Korea.");
+	printf("data=\"%s\"\n", data);
+}
+
 int main ()
 {
 	char buffer [100];
 	int cx;
+
+
+	printf("num=0x%02x\n", 9);
+	test1();
+	return 0;
 
 	cx = snprintf ( buffer, 100, "The half of %d is %d", 60, 60/2 );
 
