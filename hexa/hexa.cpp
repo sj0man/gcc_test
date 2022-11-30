4e9ab09249c52be5e76abd160027896443f0d538
Sangjin Lee <leesj@kvision.ai>
2022-11-30 16:30:01 +0900

    initial

---
+++ hexa/hexa.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 8
+
+int main(void)
+{
+	char data[256];
+	// int cam[MAX] = {0,1,2,3,4,5,6,7};
+	int cam[MAX] = {0,1,2,-1,-1,5,6,7};
+	int i;
+	unsigned int used = 0x0;
+
+	memset(data, 0, sizeof(data));
+
+
+	printf("\"");
+	for (i=0; i<MAX; i++) {
+		printf("cam[%d]=%%d, ", i);
+	}
+	printf("\\n\"\n");
+
+	for (i=0; i<MAX; i++) {
+		printf("cam[%d], ", i);
+	}
+	printf("\n");
+
+
+	return 0;
+
+	for (i=0; i<MAX; i++){
+		if(cam[i] < 0)
+			continue;
+		if(cam[i] >= 0) {
+			 if(used & (0x1U << cam[i])) {
+				cam[i] = -1;
+			 } else {
+				used |= (0x1U << cam[i]);
+			 }
+		}		
+		printf("cam[%d]=%d, 0x%08x, used=0x%08x\n", i, cam[i], (0x1U << cam[i]), used);
+	}
+}
---
+++ hexa/type.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+typedef enum {
+	VOUT_LAYOUT_NONE = 0,
+	VOUT_LAYOUT_1x1,
+	VOUT_LAYOUT_2x2,
+	VOUT_LAYOUT_3x2,
+	VOUT_LAYOUT_3x3,
+	VOUT_LAYOUT_3x3_1,	// 1 ==> 2x2
+	VOUT_LAYOUT_4x3,
+	VOUT_LAYOUT_4x3_1,	// 2 ==> 2x2
+	VOUT_LAYOUT_4x3_2,	// 1 ==> 2x2
+	VOUT_LAYOUT_4x4,
+	VOUT_LAYOUT_4x4_1,	// 1 ==> 2x2
+	VOUT_LAYOUT_4x4_2,	// 1 ==> 3x3
+	VOUT_LAYOUT_5x4,
+	VOUT_LAYOUT_5x4_1,	// 1 ==> 3x3
+	VOUT_LAYOUT_5x4_2,	// 2 ==> 2x2
+	VOUT_LAYOUT_5x4_3,	// 1 ==> 2x2
+	VOUT_LAYOUT_5x5,
+	VOUT_LAYOUT_5x5_1,	// 1 ==> 2x2
+	VOUT_LAYOUT_5x5_2,	// 1 ==> 3x3
+	VOUT_LAYOUT_6x6,
+	VOUT_LAYOUT_6x6_1,	// 1 ==> 2x2
+	VOUT_LAYOUT_6x6_2,	// 1 ==> 3x3
+	VOUT_LAYOUT_7x5,
+	MAX_VOUT_LAYOUT
+} VOUT_LAYOUT_T;
+
+int func(VOUT_LAYOUT_T layout)
+{
+    printf("%d\n", layout);
+}
+
+int main(void)
+{
+    VOUT_LAYOUT_T layout = VOUT_LAYOUT_3x3;
+    func(layout);
+}
