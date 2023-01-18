a3d5881bf087e274dcfb41511bf4718b6965f63b
Sangjin Lee <leesj@kvision.ai>
2023-01-18 14:06:19 +0900

    기능추가

--- db6/main.c
+++ db6/main.c
@@ -150,12 +150,118 @@ int db6_check(const char *fname)
 }
 
 
+void db6_dump(const char *fname)
+{
+	printf("================ sizeof struct ================\n");
+	printf("db6_head_t   =%lu\n", sizeof(db6_head_t));
+	printf("db6_date_t   =%lu\n", sizeof(db6_date_t));
+
+	int fd = -1;
+
+	// 이미 존재하는 파일
+	if ((fd = open(fname, O_RDONLY)) < 0) {
+		printf("open() fail! errno=%d,\"%s\"\n", errno, strerror(errno));
+		goto FAIL;
+	}
+
+	db6_head_t	head;
+
+	// head 읽기
+	if (pread_db6_head(fd, &head, sizeof(db6_head_t), 0) != sizeof(db6_head_t)) {
+		printf("pread_db6_head() error! errno=%d,\"%s\"\n", errno, strerror(errno));
+		goto FAIL;
+	}
+
+	printf("================ head ================\n");
+	printf("magic        =%08X\n", head.magic);
+	printf("version      =%x\n", head.version);
+	printf("num_day      =%d\n", head.num_day);
+	printf("off_date     =%d\n", head.off_date);
+	printf("off_free_date=%d\n", head.off_free_date);
+
+	db6_date_t	date;
+	memset(&date, 0, sizeof(date));
+	date.off_next = head.off_date;
+	__u32 c;
+	for (c = 0; (c < head.num_day) && date.off_next; c ++) {
+		int off_date;
+		off_date = date.off_next;
+
+		if (pread_db6_date(fd, &date, sizeof(db6_date_t), off_date) != sizeof(db6_date_t)) {
+			printf("pread_db6_date() error! errno=%d,\"%s\"\n", errno, strerror(errno));
+			goto FAIL;
+		}
+
+		printf("  ================ date=%d off=%d ================\n", c, off_date);
+		printf("  magic   =%08X\n", date.magic);
+		printf("  id      =%d\n", date.id);
+		printf("  date    =%d/%d/%d\n", date.year, date.month, date.mday);
+		printf("  off_date=%d\n", date.off_next);
+		// printf("  num_el  =%d\n", db6_num_el(&date));
+
+		int k;
+/*		
+		printf("    ================ rec ================\n");
+		for (k = 0; k < NUM_DB6_CH; k ++) {
+			int hh;
+			for (hh = 0; hh < 24; hh ++) {
+				if (date.rec[k][hh])
+					printf("    ch=%02x hour=%2d rec=%016llx\n", k, hh, date.rec[k][hh]);
+			}
+		}
+		printf("    ================ event ================\n");
+		for (k = 0; k < NUM_DB6_CH; k ++) {
+			int hh;
+			for (hh = 0; hh < 24; hh ++) {
+				if (date.event[k][hh])
+					printf("    ch=%02x hour=%2d event=%016llx\n", k, hh, date.event[k][hh]);
+			}
+		}
+		printf("    ================ num_el ================\n");
+		for (k = 0; k < NUM_EL6_TYPE; k ++) {
+			if (date.num_el[k])
+				printf("    type=%02x num_el=%d\n", k, date.num_el[k]);
+
+		}
+*/
+		printf("    ================ chunk ================\n");
+		for (k = 0; k < NUM_CHUNK; k ++) {
+			if (date.chunk[k].valid) {
+				printf("    chunk=%3d bookmark=%d hdd=%d valid=%d\n", k, date.chunk[k].bookmark, date.chunk[k].hdd, date.chunk[k].valid);
+			}
+		}
+	}
+
+	if (head.off_free_date) {
+		printf("\n\n================ off_free_date ================\n");
+
+		memset(&date, 0, sizeof(date));
+		date.off_next = head.off_free_date;
+		for (c = 0; date.off_next; c ++) {
+			int off_date;
+			off_date = date.off_next;
+
+			if (pread_db6_date(fd, &date, sizeof(db6_date_t), off_date) != sizeof(db6_date_t)) {
+				printf("pread_db6_date() error! errno=%d,\"%s\"\n", errno, strerror(errno));
+				goto FAIL;
+			}
+
+			printf("  ================ free.. date=%d off=%d next=%d ================\n", c, off_date, date.off_next);
+		}
+	}
+
+FAIL:
+	if (fd >= 0)
+		close(fd);
+}
+
 
 int main(void)
 {
 	printf("head:%ld, date:%ld\n", sizeof(db6_head_t), sizeof(db6_date_t));
-	db6_check(FNAME);
+	// db6_check(FNAME);
 
+	db6_dump(FNAME);
 	return 0;
 }
 
