5785c99448519047644c0ca031a74a021a1d6b17
Sangjin Lee <leesj@kvision.ai>
2023-03-22 10:40:08 +0900

    공백을 탭으로 변경

--- version/check.cpp
+++ version/check.cpp
@@ -3,23 +3,23 @@
 
 int getVersion(char *szVer, int& major, int& minor, int& patch, int& build)
 {
-    sscanf(szVer, "%d.%d.%d.%d", &major, &minor, &patch, &build);
-    return 0;
+	sscanf(szVer, "%d.%d.%d.%d", &major, &minor, &patch, &build);
+	return 0;
 }
 
 int main(void)
 {
-    char strVer[256] = "1.2.3.4";
-    char strVer2[256] = "10.2.30.4";
-    int major, minor, patch, build;
+	char strVer[256] = "1.2.3.4";
+	char strVer2[256] = "10.2.30.4";
+	int major, minor, patch, build;
 
-    major = minor = patch = build = -1;
+	major = minor = patch = build = -1;
 
-    getVersion(strVer, major, minor, patch, build);
-    printf("%d, %d, %d, %d\n", major, minor, patch, build);
+	getVersion(strVer, major, minor, patch, build);
+	printf("%d, %d, %d, %d\n", major, minor, patch, build);
 
-    getVersion(strVer2, major, minor, patch, build);
-    printf("%d, %d, %d, %d\n", major, minor, patch, build);
+	getVersion(strVer2, major, minor, patch, build);
+	printf("%d, %d, %d, %d\n", major, minor, patch, build);
 
-    return 0;
+	return 0;
 }
