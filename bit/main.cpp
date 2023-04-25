bc06441a273639e1a35ad57ade4712382cd83717
Sangjin Lee <leesj@kvision.ai>
2023-04-25 09:26:58 +0900

    add get_value

--- bit/main.cpp
+++ bit/main.cpp
@@ -6,8 +6,16 @@
 #include <dirent.h>
 #include <unistd.h>
 
+#define GET_VALUE(data, lbit, hbit) ((data >>lbit) & (~(~0<<(hbit-lbit+1))))
+
 typedef unsigned int	u32;
 typedef unsigned long   u64;
+
+
+void print_bit(u32 data)
+{
+	printf("data:%#x\n", GET_VALUE(data, 16, 19));
+}
 
 static void gpio_set_output(u32 pin)
 {
@@ -26,4 +34,6 @@ int main(void)
 	printf("sizeof(u64):%ld\n", sizeof(u64));
 	printf("sizeof(u32):%ld\n", sizeof(u32));
 	gpio_set_output(22);
+
+	print_bit(0x12340000);
 }
